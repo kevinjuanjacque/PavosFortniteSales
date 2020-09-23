2181af5299ac5de9d3b1c98cecb083160a97c3a2
IgnacioFajardo <ignacio.fajardor@gmail.com>
2020-09-23 14:11:17 -0300

    maquetando el home

--- front-end/salesfortnite/src/pages/Home.js
+++ front-end/salesfortnite/src/pages/Home.js
@@ -3,7 +3,7 @@ import  '../Styles/home.css'
 export const Home = () => {
     return (
         
-        
+        <>
         <div id="carousel1" className="container carousel slide" data-ride="carousel">
             <ol className="carousel-indicators">
                 <li data-target="#carousel1" data-slide-to="0" className="active"></li>
@@ -12,13 +12,13 @@ export const Home = () => {
             </ol>
             <div className="carousel-inner">
                 <div className="carousel-item active img-fluid">
-                    <img src="../assets/fondo_ps4.jpg"  className="d-block w-100" alt="imagen"  />
+                    <img src="../assets/fondo_ps4.jpg"  className="d-block w-200" alt="imagen" />
                 </div>
                 <div className="carousel-item">
-                    <img src="../assets/steam_card.png" className="d-block w-100" alt="imagen"/>
+                    <img src="../assets/steam_card.png" className="d-block w-200" alt="imagen" />
                 </div>
                 <div className="carousel-item">
-                    <img src="../assets/xbox_card.jpg" className="d-block w-100" alt="imagen"/>
+                    <img src="../assets/xbox_card.jpg" className="d-block w-200" alt="imagen" />
                 </div>
             </div>
             <a className="carousel-control-prev" href="#carousel1" role="button" data-slide="prev">
@@ -29,7 +29,86 @@ export const Home = () => {
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="sr-only">Next</span>
             </a>
+
+        </div>
+        
+        <div class="container">
+            <br></br>
+            <h5>PRODUCTOS DESTACADOS</h5>
+            <div className="separador" />
+            <br></br>
+            <div class="row row-cols" align="center">
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD</p> 
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD2</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD3</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD4</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD5</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD3</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <br></br>
+            <h5>NUEVOS PRODUCTOS</h5>
+            <div className="separador" />
+            <br></br>
+            <div class="row row-cols" align="center">
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD4</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD5</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD3</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD4</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD5</p>
+                </div>
+
+                <div class="col">
+                    <img src="../assets/psn_gift_card.jpeg" alt="imagen" height = "200"/>
+                    <p>$10 PSN GIFT CARD5</p>
+                </div>  
+            </div>
+           
+
         </div>
 
+        </>
     )
 }
