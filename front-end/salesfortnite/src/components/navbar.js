ba07126b3f10b69294691cc5639045b00c2f0df3
Kevin Juan Jacque Castillo <kevinjuanjacquecastillo@MacBook-de-Kevin.local>
2020-09-23 23:50:08 -0300

    arreglo general

--- front-end/salesfortnite/src/components/navbar.js
+++ front-end/salesfortnite/src/components/navbar.js
@@ -5,48 +5,6 @@ import '../Styles/appbar.css';
 export const Navbar = () => {
     return (
         <>
-<<<<<<< HEAD
-            <nav className="navbar colorDegradientLinear navbar-expand-lg navbar-dark sticky-top ">
-                <div class="container">
-                <Link to="/" className="navbar-brand" >
-                    <img src="../assets/logo_pavos.png"  alt="imagen" height = "75"/>
-                </Link>
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Busca tu producto..." aria-label="Search"/>
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                </form>               
-                
-                    <div className="collapse navbar-collapse" id="navbarNav" >
-                        <ul className="navbar-nav" mr-auto>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" activeClassName="active" to="/Playstation"> PlayStation </NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Xbox">Xbox One</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Nintendo">Nintendo</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Steam">Steam</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Google">Google</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Apple">Apple</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Riot">Riot</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link " activeClassName="active" to="/Fortnite">Fortnite</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                
-                <Link to="/Login" className="btn btn-light">Iniciar Sesión</Link>
-=======
             <div className=" colorDegradientLinear border-bottom-0 ">
 
                 
@@ -98,7 +56,6 @@ export const Navbar = () => {
                             <NavLink className="nav-link " activeClassName="active" to="/Fortnite">fornai</NavLink>
                         </li>
                     </ul>
->>>>>>> 0ff1210e8858e223950539875596494b8550d936
                 </div>
             </nav>
             
