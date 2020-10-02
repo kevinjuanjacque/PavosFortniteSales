b18d4d73a5cbe452e92b0a44f5279931157de2cd
Kevin Juan Jacque Castillo <kevinjuanjacquecastillo@MacBook-de-Kevin.local>
2020-10-02 01:24:26 -0300

    arreglando class por classname

--- front-end/salesfortnite/src/pages/Category.js
+++ front-end/salesfortnite/src/pages/Category.js
@@ -6,36 +6,36 @@ import { ProductoPsn } from '../components/Products/ProductoPsn';
 
 export const Category = () => {
     const { categoria }=useParams();
+    console.log(categoria)
     return (
         <>
-        
             <div className="container">
                 <br/>
                 <form className="form-inline ">
                     <div className="dropdown">
                         Ordenar por: &nbsp;
-                        <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+className                        <button className="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Precio: menor a mayor
                         </button>
-                        <div class="dropdown-menu" id="dropdownCategorias" aria-labelledby="dropdownMenuButton" onMouseLeave={()=>{
+                        <div className="dropdown-menu" id="dropdownCategorias" aria-labelledby="dropdownMenuButton" onMouseLeave={()=>{
                             document.getElementById("dropdownCategorias").click();}}>
-                            <Link className="dropdown-item" href="/Playstation"> Destacados </Link>
-                            <Link  className="dropdown-item" href="/Playstation"> Más vendidos </Link>
-                            <Link  className="dropdown-item" href="/Playstation"> Nombre: A-Z </Link>
-                            <Link  className="dropdown-item" href="/Playstation"> Nombre: Z-A </Link>
+                            <Link className="dropdown-item" to="/Playstation"> Destacados </Link>
+                            <Link  className="dropdown-item" to="/Playstation"> Más vendidos </Link>
+                            <Link  className="dropdown-item" to="/Playstation"> Nombre: A-Z </Link>
+                            <Link  className="dropdown-item" to="/Playstation"> Nombre: Z-A </Link>
                         </div>
                     </div> 
 
                     <div className="dropdown">
                         &nbsp;Filtrar por: &nbsp;
-                        <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button className="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Ninguno
                         </button>
-                        <div className="dropdown-menu" id="dropdownCategorias" aria-labelledby="dropdownMenuButton" onMouseLeave={()=>{
+                        <div className="dropdown-menu" id="dropdownCategorias" aria-labelledby="dropdownMenuButton2" onMouseLeave={()=>{
                             document.getElementById("dropdownCategorias").click();}}>
-                            <Link className="dropdown-item" href="/Playstation"> Gift cards </Link>
-                            <Link  className="dropdown-item" href="/Playstation"> Playstation Plus </Link>
-                            <Link  className="dropdown-item" href="/Playstation"> Complementos </Link>
+                            <Link className="dropdown-item" to="/Playstation"> Gift cards </Link>
+                            <Link  className="dropdown-item" to="/Playstation"> Playstation Plus </Link>
+                            <Link  className="dropdown-item" to="/Playstation"> Complementos </Link>
                         </div>
                     </div> 
                 </form>
