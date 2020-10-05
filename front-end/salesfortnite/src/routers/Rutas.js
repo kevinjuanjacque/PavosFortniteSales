f95e8b1bfde6338f4ff6ff0bc4d8c67bb8a76da8
IgnacioFajardo <ignacio.fajardor@gmail.com>
2020-10-05 03:35:18 -0300

    Agregando nosotros

--- front-end/salesfortnite/src/routers/Rutas.js
+++ front-end/salesfortnite/src/routers/Rutas.js
@@ -19,6 +19,7 @@ import { ClientScreen } from '../pages/ClientScreen';
 import { ClientScreen2 } from '../pages/ClientScreen2';
 import { MiPerfil } from '../pages/MiPerfil';
 import { SideBarLeft } from '../components/SideBars/SideBarLeft';
+import { Nosotros } from '../pages/Nosotros';
 
 
 export const Rutas = () => {
@@ -50,6 +51,8 @@ export const Rutas = () => {
                     <SideBarLeft/>
                     <Category/>
                 </Route>
+                
+                <Route exact path="/Nosotros" component={  Nosotros }/>
 
             
                 
