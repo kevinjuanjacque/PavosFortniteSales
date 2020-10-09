85b8b17b4c67289d331217ec3f921dec629f9a37
Kevin Juan Jacque Castillo <kevinjuanjacquecastillo@MacBook-de-Kevin.local>
2020-10-09 16:25:53 -0300

    se agrega backend init

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-//aca el backend
+const express = require('express');
+const app = express();
+
+app.listen('4000',()=>{
+    console.log('servidor escuchando port 4000');
+});
+
+
+
+app.get('/',(req,res)=>{
+    res.json('funcando')
+})
